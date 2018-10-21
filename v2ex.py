66682dc22074dcf676e2efa316ea98563ce0c3ce
chemf <eoyohe@gmail.com>
2018-10-21 22:40:15 +0800

    [IMP] 添加tab节点获取（未测试）

--- v2ex.py
+++ v2ex.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
+import re
 
 from workflow import Workflow3, web
 
-
 logger = None
 
-
-subtitle = '{username} • {time} • {} • '
+INPUT = ''  # 将输入作为全局变量，方便函数获取
 
 
 class KeyRegister:
@@ -32,6 +31,11 @@ class KeyRegister:
 
 @KeyRegister('hot')
 def get_hot(workflow):
+    """
+    获取 最热 tab节点的内容，v2ex开放API
+    :param workflow:
+    :return:
+    """
     data = web.get('https://www.v2ex.com/api/topics/hot.json').json()
     for item in data:
         workflow.add_item(title=item['title'], subtitle=item['content'], arg=item['url'], valid=True)
@@ -40,21 +44,68 @@ def get_hot(workflow):
 
 @KeyRegister('new')
 def get_new(workflow):
+    """
+    获取 最新 tab节点的内容，v2ex开放API
+    :param workflow:
+    :return:
+    """
     data = web.get('https://www.v2ex.com/api/topics/latest.json').json()
     for item in data:
         workflow.add_item(title=item['title'], subtitle=item['content'], arg=item['url'], valid=True)
     return len(data)
 
 
-def main(workflow):
-    key = workflow.args[0]
-    func = KeyRegister.handles.get(key, lambda x: 0)
+@KeyRegister('tab')
+def get_tab(workflow):
+    """
+    提取tab节点内容
+    :param tab: tab all
+    :return:
+    """
+    url = 'https://www.v2ex.com/?tab=%s'
+    tab = INPUT[4:]
+    response = web.get(url % tab)
+    data = parse_html(response.content)
+    for item in data:
+        workflow.add_item(title=item[1], arg=item[0], valid=True)
+    return len(data)
 
+
+def parse_html(content):
+    """
+    提取v2ex页面文章标题和链接
+    :param content: 标题内容
+    :return: [(url, title)]
+    """
+    data = re.compile(r'<span class="item_title"><a href="(.*)">(.*)</a></span>').findall(content)
+    return data
+
+
+def dispatch(input=''):
+    """
+    指令判断
+    :param input:
+    :return:
+    """
+    # 如果是主题节点
+    logger.error(INPUT)
+    if input.startswith('tab '):
+        func = KeyRegister.handles.get('tab')
+    else:
+        func = KeyRegister.handles.get(input, lambda x: 0)
+    return func
+
+
+def main(workflow):
+    global INPUT
+    INPUT = workflow.args[0]
+
+    func = dispatch(INPUT)
     data_len = func(workflow)
 
     # 如果没有items，添加默认items
     if data_len == 0:
-        workflow.add_item(title=key, subtitle=key, arg=key, valid=True)
+        workflow.add_item(title=INPUT, subtitle=INPUT, arg=INPUT, valid=True)
 
     workflow.send_feedback()
 
@@ -63,5 +114,3 @@ if __name__ == '__main__':
     wf = Workflow3()
     logger = wf.logger
     sys.exit(wf.run(main))
-
-
